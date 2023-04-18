4b5e9b230b7d0a348843b22119e3a7be93708e67
MiguelGuedes1 <miguelsilvaguedes1@gmail.com>
2023-04-19 00:24:45 +0100

    Footer and final adjusts

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function abrirmenu(){
     var barramenuaberto=document.getElementsByClassName("barramenu")[0]
 
     if(barramenuaberto.style.width=="0px"){
-        barramenuaberto.style.width="180px"
+        barramenuaberto.style.width="140px"
     }
     else{
         barramenuaberto.style.width="0px"
