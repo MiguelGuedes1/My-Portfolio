09c3837c47a95238f53222ca7e3c54e78bd6bc5b
MiguelGuedes1 <miguelsilvaguedes1@gmail.com>
2023-04-27 01:10:00 +0100

    adding 1 more project and fix nav bar

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 function abrirmenu(){
-    var barramenuaberto=document.getElementsByClassName("barramenu")[0]
+    var barramenuaberto=document.getElementsByClassName("barramenu")[0];
+    
+    barramenuaberto.style.width = barramenuaberto.style.width || "0px";
+    // Esta linha define a largura da barra de menu como "0px" se ainda não tiver sido definida.
 
     if(barramenuaberto.style.width=="0px"){
-        barramenuaberto.style.width="140px"
-    }
-    else{
-        barramenuaberto.style.width="0px"
+        barramenuaberto.style.width="140px";
+    } else{
+        barramenuaberto.style.width="0px";
     }
 
 }
